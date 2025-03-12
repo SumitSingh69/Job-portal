1cdcadfce9fe9926ecdf8e6daf343963beabe9ec
Pawan Dasila <pawandasila06@gmail.com>
2025-03-12 21:24:22 +0530

    fixed bug

--- server/middleware/asyncHandler.js
+++ server/middleware/asyncHandler.js
@@ -1,3 +1,5 @@
+import { HTTPSTATUS } from "../config/https.config";
+
 const asyncHandler = (fn) => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch((error) => {
       console.log(`Error occurred at path ${req.path}:`, error);
